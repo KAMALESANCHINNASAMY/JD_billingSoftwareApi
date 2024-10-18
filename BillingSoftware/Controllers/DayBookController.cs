d164855308f447d1fd170901b321d59bf3f58ab0
KAMALESANCHINNASAMY <kamalesan2002@gmail.com>
2024-10-18 13:54:00 +0530

    Code changes

---
+++ BillingSoftware/Controllers/DayBookController.cs
@@ -0,0 +1,73 @@
+﻿using BillingSoftware.Helpers;
+using BillingSoftware.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Data;
+using System;
+
+namespace BillingSoftware.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DayBookController : ControllerBase
+    {
+        private readonly IOptions<DBModel> appSettings;
+        public IConfiguration Configuration { get; }
+        string Conn = string.Empty;
+        private string errorCode;
+        private object errorDesc;
+        public DayBookController(IConfiguration configuration, IOptions<DBModel> appSettings)
+        {
+            Configuration = configuration;
+            appSettings = appSettings;
+            Conn = new Database().GetConnectionString();
+        }
+
+        [HttpGet]
+        public List<DayBookModel> get_daybook_bydate(int companyid, string fromdate, string todate)
+        {
+            DataTable dtData = null;
+            List<DayBookModel> mItems = new List<DayBookModel>();
+            SqlDataAdapter adapter = null;
+            string jsonData = string.Empty;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Conn))
+                {
+                    SqlCommand cmd = new SqlCommand("get_daybook_bydate", con);
+                    cmd.Parameters.AddWithValue("@companyid", companyid);
+                    cmd.Parameters.AddWithValue("@fromdate", fromdate);
+                    cmd.Parameters.AddWithValue("@todate", todate);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    adapter = new SqlDataAdapter(cmd);
+                    dtData = new DataTable();
+                    adapter.Fill(dtData);
+                    if (dtData.Rows.Count > 0)
+                    {
+                        mItems = Helper.ConvertToList<DayBookModel>(dtData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return mItems;
+        }
+    }
+}
+
+public class DayBookModel
+{
+    public string date { get; set; }
+    public string description { get; set; }
+    public string payment_type { get; set; }
+    public string bank_name { get; set; }
+    public decimal income { get; set; }
+    public decimal expense { get; set; }
+}
