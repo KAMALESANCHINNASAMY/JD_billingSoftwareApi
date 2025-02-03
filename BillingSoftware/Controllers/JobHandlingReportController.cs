b3a9a4820c81a048984408a6a03cf6bf700aea1d
KAMALESANCHINNASAMY <kamalesan2002@gmail.com>
2025-02-03 09:22:09 +0530

    Code changes

--- BillingSoftware/Controllers/JobHandlingReportController.cs
+++ BillingSoftware/Controllers/JobHandlingReportController.cs
@@ -97,5 +97,39 @@ namespace BillingSoftware.Controllers
             }
             return mItems;
         }
+
+
+        [HttpGet]
+        public List<LedgerModel> get_purchase_from_thirdparty_legger(int third_partyid, string fromdate, string todate)
+        {
+            DataTable dtData = null;
+            List<LedgerModel> mItems = new List<LedgerModel>();
+            SqlDataAdapter adapter = null;
+            string jsonData = string.Empty;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Conn))
+                {
+                    SqlCommand cmd = new SqlCommand("get_purchase_from_thirdparty_legger", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@third_partyid", third_partyid);
+                    cmd.Parameters.AddWithValue("@fromdate", fromdate);
+                    cmd.Parameters.AddWithValue("@todate", todate);
+                    con.Open();
+                    adapter = new SqlDataAdapter(cmd);
+                    dtData = new DataTable();
+                    adapter.Fill(dtData);
+                    if (dtData.Rows.Count > 0)
+                    {
+                        mItems = Helper.ConvertToList<LedgerModel>(dtData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return mItems;
+        }
     }
 }
